1412d778da1c374bc3575a033c0e3c27562f062d
bivas-work <itsbivasworkemail@gmail.com>
2025-02-17 13:52:11 +0000

    leet code challenge 75, day 5 of prep

--- src/TravellingSalesmanProblem.java
+++ src/TravellingSalesmanProblem.java
@@ -1,5 +1,7 @@
 public class TravellingSalesmanProblem {
     public static void main (String[] args) {
-
+        Integer a = new Integer(826);
+        Integer b = new Integer(826);
+        System.out.println(a.intValue()==b.intValue());
     }
 }
